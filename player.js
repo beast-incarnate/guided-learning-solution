4834407746d32d51fbe28044ddd475ff35a338c7
kunsingh7 <singh.kunal112@gmail.com>
2021-02-10 16:12:52 +0530

    load guide response

--- player.js
+++ player.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
     const stTipCssUrl = "https://guidedlearning.oracle.com/player/latest/static/css/stTip.css";    
+    const guideUrl = "https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=func&refresh=true&env=dev&type=startPanel&vars%5Btype%5D=startPanel&sid=none&_=1582203987867";
+    
     loadStTipCss(stTipCssUrl);
+    loadGuideAndUpdateDom(guideUrl);
  
  });
 
@@ -16,5 +19,20 @@ $(document).ready(function(){
         error: function(err) {
             console.log(err);
         }                  
+    });
+}
+
+
+function loadGuideAndUpdateDom(url) {
+    $.ajax({
+        url: url,
+        type: "GET",
+        dataType: 'text',
+        success : function(data) {
+            console.log(data);            
+        },
+        error : function(err) {
+            console.log(err);
+        }
     });
 }
